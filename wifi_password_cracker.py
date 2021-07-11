8173cbaa505c424028e9d9a5628f95b5957d3a1c
Randeep Singh <74869072+randeepsingh22@users.noreply.github.com>
2021-07-11 20:17:55 +0530

    Update wifi_password_cracker.py

--- wifi_password_cracker.py
+++ wifi_password_cracker.py
@@ -1,33 +1,17 @@
 import subprocess
 
-# Import the re module so that we can make use of regular expressions. 
+ 
 import re
 
-# Python allows us to run system commands by using a function provided by the subprocess module 
-# (subprocess.run(<list of command line arguments goes here>, <specify the second argument if you want to capture the output>))
-# The script is a parent process and creates a child process which runs the system command, 
-# and will only continue once the child process has completed.
-# To save the contents that gets sent to the standard output stream (the terminal) 
-# we have to specify that we want to capture the output, 
-# so we specify the second argument as capture_output = True. 
-# This information gets stored in the stdout attribute. 
-# The information is stored in bytes and we need to decode it to Unicode 
-# before we use it as a String in Python.
+
 command_output = subprocess.run(["netsh", "wlan", "show", "profiles"], capture_output = True).stdout.decode()
 
-# We imported the re module so that we can make use of regular expressions. 
-# We want to find all the Wifi names which is always listed after "ALL User Profile     :". 
-# In the regular expression we create a group of all characters until the return escape sequence (\r) appears.
+
 profile_names = (re.findall("All User Profile     : (.*)\r", command_output))
 print(profile_names)
 
-# We create an empty list outside of the loop where dictionaries with all the wifi 
-# username and passwords will be saved.
 wifi_list = list()
 
-# If we didn't find profile names we didn't have any wifi connections, 
-# so we only run the part to check for the details of the wifi and 
-# whether we can get their passwords in this part.
 if len(profile_names) != 0:
     for name in profile_names:
         # Every wifi connection will need its own dictionary which will be appended to the wifi_list
